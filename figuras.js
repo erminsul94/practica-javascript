ed6fe471fb5e483ae2b83b24ec59e4ed162dd93a
Erminsul Quintero <erminsulquintero@gmail.com>
2022-06-12 23:42:52 -0500

    iniciando archivos intento con formulas 2

--- figuras.js
+++ figuras.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+// console.log('hello world')
+// Cuadrado
+const ladocuadrado = 5;
+console.log('Los lados del cuadrado miden: ' + ladocuadrado);
+
+const perimetroCuadrado = ladocuadrado * 4;
+console.log('el perimetro del cuadrado es: ' + perimetroCuadrado);
+
+const areaCuadrado = ladocuadrado * ladocuadrado;
+console.log('el area del cuadrado es: ' + areaCuadrado);
+
+// Triangulo
+
+const ladoTriangulo1 = 5;
+console.log('Los lados del cuadrado miden: ' + ladoTriangulo1);
