9e4780316a26df32ef6fd26c212050b533611c9a
Erminsul Quintero <erminsulquintero@gmail.com>
2022-06-13 00:24:43 -0500

    se agregaron mas formulas

--- figuras.js
+++ figuras.js
@@ -1,5 +1,7 @@
 // console.log('hello world')
 // Cuadrado
+
+console.group('Cuadrados')
 const ladocuadrado = 5;
 console.log('Los lados del cuadrado miden: ' + ladocuadrado);
 
@@ -8,8 +10,57 @@ console.log('el perimetro del cuadrado es: ' + perimetroCuadrado);
 
 const areaCuadrado = ladocuadrado * ladocuadrado;
 console.log('el area del cuadrado es: ' + areaCuadrado);
-
+console.groupEnd();
 // Triangulo
 
-const ladoTriangulo1 = 5;
-console.log('Los lados del cuadrado miden: ' + ladoTriangulo1);
+console.group('Triangulos')
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    'Los lados del triangulo miden: ' 
+    + ladoTriangulo1 
+    + "cm, " 
+    + ladoTriangulo2 
+    + "cm, " 
+    + baseTriangulo 
+    + "cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log('La altura del triangulo es: ' + alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log('El perimetro del triangulo es: ' + perimetroTriangulo);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log('El area del triangulo es: ' + areaTriangulo);
+
+console.groupEnd();
+
+// Circulo
+
+console.group('Circulos');
+
+// Radio
+const radioCirculo = 4;
+console.log('El radio del triangulo es: ' + radioCirculo);
+
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log('El diametro del circulo es: ' + diametroCirculo);
+
+// PI
+const PI = Math.PI;
+console.log('El valor de PI es: ' + PI);
+
+// Circuferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log('El permetro del circulo es: ' + perimetroCirculo);
+
+// Area
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log('El area del circulo es: ' + areaCirculo);
+
+console.groupEnd();
